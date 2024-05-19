229480230822241504bbeb4019b3e76b9225d7f4
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-05-19 23:41:31 +0400

    some fixes

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,20 +7,12 @@ request(url, (error, response, body) => {
   if (error) console.log(error);
   else {
     const zor = JSON.parse(body);
-    const obj = {
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-      7: 0,
-      8: 0,
-      9: 0,
-      10: 0
-    };
+    const obj = {};
     for (let i = 0; i < zor.length; i++) {
-      if (zor[i].completed) {
+      if (zor[i].completed && !obj[zor[i].userId]) {
+        obj[zor[i].userId] = 1;
+      }
+      else if (zor[i].completed) {
         obj[zor[i].userId] += 1;
       }
     }
